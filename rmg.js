9fd47c121f3778201bcda3c8fbfeb635336a18c7
SpruceGuy1 <139022212+SpruceGuy1@users.noreply.github.com>
2025-11-22 13:25:48 -0500

    Create rmg.js

---
+++ rmg.js
@@ -0,0 +1,10 @@
+
+export function RMG(){
+ var terrainTypes = ["g", "m", "w", "f"]
+ var map = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []]
+  for(let i = 0;i<=15;i++){
+    for(let j = 0;j<=15;i++){
+      map[i][j]=terrainTypes[Math.floor(Math.random() * terrainTypes.length)]
+    }
+  }
+}
