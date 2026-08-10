c1559b743cb92278fba267ad99961dd9189b4c19
SpruceGuy1 <SpruceGuy1@users.noreply.github.com>
2026-08-10 11:04:36 +0000

    chore: format code with Prettier

--- rmg.js
+++ rmg.js
@@ -1,11 +1,12 @@
-
-export function RMG(){
- var terrainTypes = ["g", "m", "w", "f"]
- var map = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []]
-  for(let i = 0;i<=15;i++){
-    for(let j = 0;j<=15;j++){
-      map[i].push(terrainTypes[Math.floor(Math.random() * terrainTypes.length)])
+export function RMG() {
+  var terrainTypes = ["g", "m", "w", "f"];
+  var map = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []];
+  for (let i = 0; i <= 15; i++) {
+    for (let j = 0; j <= 15; j++) {
+      map[i].push(
+        terrainTypes[Math.floor(Math.random() * terrainTypes.length)],
+      );
     }
   }
- return map
+  return map;
 }
