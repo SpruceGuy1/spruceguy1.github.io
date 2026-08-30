6062f9f5387e42d753a5c2e3e5487aca3ad8460f
SpruceGuy1 <SpruceGuy1@users.noreply.github.com>
2026-08-30 14:30:37 +0000

    chore: format code with Prettier

--- modules/csd/boat.js
+++ modules/csd/boat.js
@@ -1,13 +1,12 @@
-
-export function createBoat(state, boat){
-
-    $("#c"+state.loc).append("<div class='boat origin-"+state.loc+"' id='boat-"+boat.length+"'>")
-    boat.push({
-        origin:state.loc,
-        id:boat.length,
-        y:Math.floor(state.loc/8),
-        x:state.loc - Math.floor(state.loc/8)*8
-
-    })
-    //$("#"+boat[boat.length-1].id).css(TO BE DETERMINED)
-} 
+export function createBoat(state, boat) {
+  $("#c" + state.loc).append(
+    "<div class='boat origin-" + state.loc + "' id='boat-" + boat.length + "'>",
+  );
+  boat.push({
+    origin: state.loc,
+    id: boat.length,
+    y: Math.floor(state.loc / 8),
+    x: state.loc - Math.floor(state.loc / 8) * 8,
+  });
+  //$("#"+boat[boat.length-1].id).css(TO BE DETERMINED)
+}
