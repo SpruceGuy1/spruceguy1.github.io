512d1d7cbac8c037597338ad68a2d2403f126417
SpruceGuy1 <139022212+SpruceGuy1@users.noreply.github.com>
2026-09-06 12:51:19 +0000

    autocommit.sh

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Append the function to the real user .bashrc if it isn't already there
+#!/bin/bash
+# Append the function to the real user .bashrc if it isn't already there
+if ! grep -q "my_function" ~/.bashrc; then
+  cat << 'EOF' >> ~/.bashrc
+
+autocommit() {
+    bash /commit.sh $1
+}
+EOF
+fi
