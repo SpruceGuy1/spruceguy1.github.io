993076bc452f027d83bb7b93d856e0abc6e776a2
SpruceGuy1 <139022212+SpruceGuy1@users.noreply.github.com>
2026-09-11 11:41:33 +0000

    Revert "Merge branch 'ferbingo' of https://github.com/SpruceGuy1/spruceguy1.github.io into ferbingo"
    This reverts commit 9ce9241ab972c85bbc58d5154a9af3a3b791b673, reversing
    changes made to 0c673e60772d221312b0c580b4967465d1846cb0.

--- functions/scripts/sync-questions.js
+++
@@ -1,8 +0,0 @@
-const { copyFileSync } = require("node:fs");
-const { join } = require("node:path");
-
-const functionsDirectory = join(__dirname, "..");
-copyFileSync(
-  join(functionsDirectory, "..", "ferbingo", "questions.json"),
-  join(functionsDirectory, "questions.json"),
-);
